cd0cfb225837ce20ae38bc7d7fc131787aaf965f
Andrew Garmon <andrewgarmon@ufl.edu>
2021-09-19 15:11:59 -0400

    Added CreateAccountActivity

--- app/app/src/main/java/com/example/powerpal/CreateAccountActivity.java
+++ app/app/src/main/java/com/example/powerpal/CreateAccountActivity.java
@@ -1,12 +1,35 @@
 package com.example.powerpal;
 
-import android.os.Bundle;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
 public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account);
+    }
+    public void login(View view) {
+        // validate emails
+        // validate password
+        // send to database
+        // if unsuccessful, display error
+
+        new AlertDialog.Builder(this)
+                .setTitle("Not implemented")
+                .setMessage("We haven't implemented this yet..")
+                .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setIcon((android.R.drawable.ic_dialog_alert))
+                .show();
     }
 }
