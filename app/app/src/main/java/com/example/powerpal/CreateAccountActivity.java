7b56d7bec62cb54935fb53b7c4ed33981bbbab13
Andrew Garmon <andrewgarmon@ufl.edu>
2021-09-19 14:30:10 -0400

    Added help button with information to main activity

---
+++ app/app/src/main/java/com/example/powerpal/CreateAccountActivity.java
@@ -0,0 +1,12 @@
+package com.example.powerpal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/app/src/main/java/com/example/powerpal/MainActivity.java
+++ app/app/src/main/java/com/example/powerpal/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.powerpal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createAccount(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
     }
     public void login(View view) {
 
+
     }
     public void showAbout(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("About PowerPal")
+                .setMessage(R.string.about)
+                .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
+                    }
+                })
+                .setIcon((android.R.drawable.ic_dialog_info))
+                .show();
     }
 }
