fa140acde78ddd3b7a904e80ecdf1c53f7ba17d0
Andrew Garmon <andrewgarmon@ufl.edu>
2021-09-19 12:31:16 -0400

    Added basic ui and empty functions for main activity

--- app/app/src/main/java/com/example/powerpal/MainActivity.java
+++ app/app/src/main/java/com/example/powerpal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.powerpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createAccount(View view) {
+    }
+    public void login(View view) {
+
+    }
+    public void showAbout(View view) {
+
     }
 }
