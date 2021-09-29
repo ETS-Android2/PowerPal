b2a9b7746a94a84f2935eacd783afcea1f25766f
D. Caine Winters <63658726+elcaine@users.noreply.github.com>
2021-09-29 10:35:34 -0400

    Put test code into test class

--- app/src/androidTest/java/com/example/powerpal/WelcomeActivityTest.java
+++ app/src/androidTest/java/com/example/powerpal/WelcomeActivityTest.java
@@ -2,6 +2,17 @@ package com.example.powerpal;
 
 import static org.junit.Assert.*;
 
-public class WelcomeActivityTest {
+import android.content.Context;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+public class WelcomeActivityTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.powerpal", appContext.getPackageName());
+    }
 }
