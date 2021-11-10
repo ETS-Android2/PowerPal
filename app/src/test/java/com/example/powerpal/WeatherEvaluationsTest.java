2ce7b715979d32a65e976169437ce67129ad7e9d
D. Caine Winters <63658726+elcaine@users.noreply.github.com>
2021-11-10 12:44:52 -0500

    Getting app tests into CI.

--- app/src/test/java/com/example/powerpal/WeatherEvaluationsTest.java
+++ app/src/test/java/com/example/powerpal/WeatherEvaluationsTest.java
@@ -25,6 +25,6 @@ public class WeatherEvaluationsTest {
     @Test
     public void rainYesOrNoTEST() {
         WeatherEvaluations x = new WeatherEvaluations();
-        assertSame("yes", x.rainYesOrNo(85, 10, 69));
+        assertSame("yesP", x.rainYesOrNo(85, 10, 69));
     }
 }
